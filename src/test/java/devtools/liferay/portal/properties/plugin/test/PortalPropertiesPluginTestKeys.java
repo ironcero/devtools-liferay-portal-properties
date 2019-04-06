b1baaea7d7c84b69da0df9e081064e519f5c2708
Ignacio Roncero Bazarra <ironcero@gmail.com>
2019-04-06 09:22:11 +0200

    Changed groupId to upload to SonaType repository
    Changed groupId to upload to SonaType repository

--- src/test/java/devtools/liferay/portal/properties/plugin/test/PortalPropertiesPluginTestKeys.java
+++ src/test/java/devtools/liferay/portal/properties/plugin/test/PortalPropertiesPluginTestKeys.java
@@ -14,7 +14,7 @@ public class PortalPropertiesPluginTestKeys {
             "        }\n" +
             "    }\n" +
             "    dependencies {\n" +
-            "        classpath group: 'devtools.liferay', name: 'portal-properties', version: '1.1.1'\n" +
+            "        classpath group: 'com.github.ironcero', name: 'portal-properties', version: '2.0.0'\n" +
             "    }\n" +
             " }\n" +
             " apply plugin: 'devtools-liferay-portal-properties'\n" +
